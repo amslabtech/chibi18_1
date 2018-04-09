16d9ce37de13ba268bafc28f4a6b44c982b36b05
arima <arijun0307@gmail.com>
2018-04-09 18:32:48 +0900

    waypointをrvizで見る用のpose array をpublish

--- src/sending_goal.cpp
+++ src/sending_goal.cpp
@@ -1,7 +1,9 @@
 #include <ros/ros.h>
 #include <tf/tf.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/PoseArray.h>
 std::vector<geometry_msgs::PoseStamped> wp;
+geometry_msgs::PoseArray goals;
 void set_pose(float, float, float);
 
 int main(int argc, char** argv){
@@ -11,19 +13,22 @@ int main(int argc, char** argv){
 	ros::Rate loop_rate(10);
 	 
 	ros::Publisher pose_pub = nh.advertise<geometry_msgs::PoseStamped>("/move_base_simple/goal", 100);
-	
+  ros::Publisher poses_pub = nh.advertise<geometry_msgs::PoseArray>("waypoints",100);
 	set_pose(14.4, 0.0, -1.55);
 	set_pose(14.9, -13.8, -3.08);
 	set_pose(-18.7, -15.3, 1.62);
 	set_pose(-19.1, -1.1, 0.0);
 	set_pose(0.0, 0.0, 0.0);
-	 
+	goals.header.frame_id = "map";
+
 	for(int i=0;i<wp.size();i++){
 		ros::Duration(2).sleep();
 		pose_pub.publish(wp[i]);
+		goals.poses.push_back(wp[i].pose);
 	}
 	
 	while(ros::ok()){
+		poses_pub.publish(goals);
 		ros::spinOnce();
 		loop_rate.sleep();
 	}
